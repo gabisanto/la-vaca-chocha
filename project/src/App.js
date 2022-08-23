67e7399adb639d6d4f65fda20674dbe3ce05ebb6
DeboraLedo <ledo.debora@gmail.com>
2022-08-23 17:55:22 -0300

    "primer commit"

--- project/src/App.js
+++ project/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import ProductsCard from "./components/ProductsCard";
 
 const App = () => {
   return (
     <div>
       {/* <Navbar /> */}
       <div>
+      
+        <ProductsCard/>
         <Routes>
           {/* <Route path="/" element={<Home />} />
           <Route path="404" element={<NotFound />} />
