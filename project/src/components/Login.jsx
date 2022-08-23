6ceca06e663424ffec44cd66ce1eca7fb7ffd55c
gabisanto <gabrielasantoriello@gmail.com>
2022-08-23 17:53:47 -0300

    formularios de login, register, create, y rutas

---
+++ project/src/components/Login.jsx
@@ -0,0 +1,141 @@
+import { useState } from "react";
+import useMatches from "../hooks/useMatches";
+import back from "../assets/backusers.jpg";
+import AlertMessage from "../commons/AlertMessage";
+/* import { useDispatch } from "react-redux"; */
+import { useNavigate } from "react-router";
+import { useForm } from "react-hook-form";
+/* import { sendLoginRequest } from "../store/user"; */
+import { TextField, Container, Box, Button } from "@mui/material";
+import { AccountCircle, Visibility } from "@mui/icons-material";
+import styles from "../styles/userpages.module.css";
+
+const Login = () => {
+  /* media queries custom hook */
+  const matches = useMatches();
+
+  /* inicio proceso de login */
+  const [loginStatus, setLoginStatus] = useState("");
+
+  const navigate = useNavigate();
+  /*   const dispatch = useDispatch(); */
+  /* configuración usando react hook form */
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  /* realizando el login */
+  const onSubmit = (data) => {
+    console.log(data);
+    /* dispatch(sendLoginRequest(data))
+      .then(({ payload }) => {
+        if (payload) {
+          setLoginStatus("success");
+          localStorage.setItem("user", JSON.stringify(payload));
+          setTimeout(() => navigate("/"), 3000);
+        } else {
+          setLoginStatus("error");
+          setTimeout(() => setLoginStatus(""), 3000);
+          reset();
+        }
+      })
+      .catch(() => navigate("/404")); */
+  };
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${back})`,
+      }}
+      className={styles.backImg}
+    >
+      <Container
+        maxWidth={matches ? "xs" : "m"}
+        sx={{
+          p: 1,
+          mb: 1,
+          backgroundColor: "#e0e0e0",
+          borderRadius: 1,
+          color: "action.active",
+          fontWeight: "bold",
+        }}
+      >
+        <p style={{ textAlign: "center" }}>Ingreso al sitio</p>
+      </Container>
+      <Container
+        maxWidth={matches ? "xs" : "m"}
+        sx={{ p: 5, backgroundColor: "#e0e0e0", borderRadius: 1 }}
+      >
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "flex-end",
+            }}
+            mb={2}
+          >
+            <AccountCircle sx={{ color: "action.active", mr: 1, my: 0.5 }} />
+            <TextField
+              id="input-with-sx"
+              name="email"
+              fullWidth
+              label="E-mail"
+              variant="standard"
+              {...register("email", {
+                required: "Required field",
+              })}
+              error={!!errors?.email}
+              helperText={errors?.email ? errors.email.message : null}
+            />
+          </Box>
+          <Box sx={{ display: "flex", alignItems: "flex-end" }} mb={2}>
+            <Visibility sx={{ color: "action.active", mr: 1, my: 0.5 }} />
+            <TextField
+              id="input-with-sx"
+              name="password"
+              type="password"
+              fullWidth
+              label="Contraseña"
+              variant="standard"
+              {...register("password", {
+                required: "Required field",
+              })}
+              error={!!errors?.password}
+              helperText={errors?.password ? errors.password.message : null}
+            />
+          </Box>
+
+          <Button
+            type="submit"
+            variant="contained"
+            fullWidth
+            size="large"
+            sx={{
+              fontWeight: "bold",
+              backgroundColor: "#03A696",
+              "&:hover": {
+                backgroundColor: "#04BF9D",
+                color: "#757575",
+              },
+            }}
+          >
+            Ingresar
+          </Button>
+        </form>
+        {loginStatus && (
+          <AlertMessage
+            type={loginStatus}
+            message={
+              loginStatus === "success"
+                ? `Bienvenid@ al sitio!`
+                : `Por favor verifique sus credenciales`
+            }
+          />
+        )}
+      </Container>
+    </div>
+  );
+};
+
+export default Login;
