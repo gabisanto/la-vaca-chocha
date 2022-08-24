2de1d41c9c34c426eb9349ae3616292b4085d93e
santimenasa <santimenasa@gmail.com>
2022-08-24 18:24:26 -0300

    navbar

---
+++ project/src/components/Navbar.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Stack,
+  Button
+} from "@mui/material";
+import LoginIcon from "@mui/icons-material/Login";
+import AssignmentIndIcon from "@mui/icons-material/AssignmentInd";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import InventoryIcon from "@mui/icons-material/Inventory";
+
+const Navbar = () => {
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h5"
+            component="div"
+            textAlign="left"
+            sx={{ flexGrow: 1 }}
+          >
+            La Vaca Chocha
+          </Typography>
+          <Stack direction="row" spacing={4}>
+            <Button color="inherit">
+              {" "}
+              <AssignmentIndIcon />
+              registrarse{" "}
+            </Button>
+            <Button color="inherit">
+              <LoginIcon /> LOGIN
+            </Button>
+            <Button color="inherit">
+              <InventoryIcon /> productos
+            </Button>
+            <Button color="inherit">
+              <ShoppingCartIcon /> carrito
+            </Button>
+          </Stack>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Navbar;
+// 1) npm install @mui/material @emotion/react @emotion/styled   
+/*  2)"esto va en index.js arriba del title"  =====> 
+ 
+<link rel="stylesheet"
+ href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+/>  
+
+3) instalar iconos ====> npm install @mui/icons-material
+*/
