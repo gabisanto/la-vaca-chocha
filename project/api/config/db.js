2ad5fcf1a9f2c23bf5bae3c71c2bfcce7f4d8661
luampelus <luampelus@gmail.com>
2022-08-24 12:27:40 -0300

    rutas de cart

--- project/api/config/db.js
+++ project/api/config/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize("lavacachocha", "postgres", "luayher", {
+const db = new Sequelize("lavacachocha", null, null, {
   host: "localhost",
   dialect: "postgres",
   logging: false,
