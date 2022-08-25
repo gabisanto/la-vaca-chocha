a73ef66731ea89b5424919b5b2a89dc838b7cbc3
Alejandro Ramos <caramos336@gmail.com>
2022-08-25 11:22:15 -0300

    Rutas De Categorias

--- project/api/routes/category.js
+++ project/api/routes/category.js
@@ -1,4 +1,42 @@
 const express = require("express");
+const { Products } = require("../models");
 const router = express.Router();
+const Categories = require("../models/Categories")
 
-module.exports = router;
+
+//RUTA QUE DEVUELVA LOS PRODUCTOS DE DETERMINADA CATEGORIA
+
+router.get('/:name', function(req, res, next) {
+    Categories.findOne({
+        where: {
+            name : req.params.name 
+        },
+        include : {model : Products} 
+    })
+    .then((products)=>res.send(products))
+    .catch(next)
+}); 
+
+
+
+//RUTA QUE DEVUELVE TODAS LAS CATEGORIAS
+
+router.get('/', function(req, res, next) {
+    Categories.findAll()
+    .then((products)=>res.send(products))
+    .catch(next)
+}); 
+
+
+
+//RUTA QUE DEVUELVA LOS PRODUCTOS CUYO NOMBRE, COINCIDA CON LA BUSQUEDA DEL USUARIO A TRAVES DE INPUT
+
+router.get('/name',function(req,res,next){
+
+});
+
+
+
+
+
+
--- project/api/routes/products.js
+++ project/api/routes/products.js
@@ -2,12 +2,15 @@ const express = require("express");
 const router = express.Router();
 const Products = require("../models/Products.js")
 
+//RUTA QUE DEVUELVA TODOS LOS PRODUCTOS
 
 router.get('/', function(req, res, next) {
     Products.findAll()
     .then((products)=>res.send(products))
     .catch(next)
-})   
+}) 
+
+//RUTA QUE DEVUELVA UN PRODUCTO ESPECIFICO
 
 router.get('/:id', function(req, res, next) {
     Products.findOne({
@@ -16,9 +19,9 @@ router.get('/:id', function(req, res, next) {
         }
     })
     .then((product)=>res.send(product))
-    
 })  
 
+//RUTA PARA AGREGAR UN PRODUCTO
 
 router.post('/', function(req,res,next) {
     Products.create (
@@ -26,6 +29,7 @@ router.post('/', function(req,res,next) {
     )
     .then((product)=>res.send(product))
 
+// RUTA PARA ELIMINAR UN PRODUCTO    
 
 router.delete('/:id',function(req,res,next){
     Products.destroy({
@@ -34,6 +38,8 @@ router.delete('/:id',function(req,res,next){
         }
     }).then((user)=> res.sendStatus(202))
 })
+
+//RUTA PARA MODIFICAR UN PRODUCTO
 
 router.put('/:id',function(req,res,next){
     Products.update(req.body, {where : {id : req.params.id}, returning : true })
